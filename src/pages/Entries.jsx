afea3e3a3dd60193a81d7e434318974ac55a0131
clemensFH <clemens.hoerhan@stud.fh-campuswien.ac.at>
2024-01-03 20:18:02 +0100

    Entries basic version

--- src/pages/Entries.jsx
+++ src/pages/Entries.jsx
@@ -1,16 +1,71 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar';
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { useState, useEffect } from "react";
+import '../styles/Entries.css';
+//import { getFinancialEntriesFromDB } from "firebase";
 
-//Clemens
-const Entries = () => {
-    return (
-        <div className='Entries'>
-            <Sidebar></Sidebar>
-            <div className='content'>
-                <h1>Entries</h1>
-            </div>
-        </div>
-    )
+function Entry({entry}) {
+  return (
+    <div className="entry">
+      <h4>{entry.party}</h4>
+      <p>{entry.description}</p>
+      <p>{entry.time}</p>
+      <p>{entry.amount} €</p>
+    </div>
+  );
 }
 
-export default Entries
+const entry1 = {
+  party: "Wallmart",
+  description: "Expense for food and drinks",
+  time: "2023-01-15T18:30:00",
+  amount: 50.0,
+};
+
+const entry2 = {
+  party: "Taco Bell",
+  description: "Shopping for groceries",
+  time: "2023-01-16T12:45:00",
+  amount: 30.0,
+};
+
+const entry3 = {
+  party: "XNXX",
+  description: "Gas bill payment",
+  time: "2023-01-17T09:15:00",
+  amount: 40.0,
+};
+
+const Entries = () => {
+  const [financialEntries, setFinancialEntries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      //const entries = getFinancialEntriesFromDB();
+      const entries = [entry1, entry2, entry3];
+      setFinancialEntries(entries);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Entries">
+      <Sidebar></Sidebar>
+      <div className="content">
+        <h1>Entries</h1>
+        <div className="entry-container">
+          <ul>
+            {financialEntries.map((entry, index) => (
+              <li key={index}>
+                <Entry entry={entry} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Entries;
