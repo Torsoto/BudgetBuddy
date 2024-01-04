a27be99e4bed1bae6a772809fe920d82596df0ef
clemensFH <clemens.hoerhan@stud.fh-campuswien.ac.at>
2024-01-04 22:29:48 +0100

    function buttons base

--- src/pages/Entries.jsx
+++ src/pages/Entries.jsx
@@ -1,39 +1,64 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
 import { useState, useEffect } from "react";
-import '../styles/Entries.css';
+import "../styles/Entries.css";
 //import { getFinancialEntriesFromDB } from "firebase";
 
-function Entry({entry}) {
+function Entry({ entry }) {
   return (
     <div className="entry">
       <h4>{entry.party}</h4>
       <p>{entry.description}</p>
       <p>{entry.time}</p>
       <p>{entry.amount} €</p>
+      <p></p>
+      <button>Edit</button>
+      <button onClick={() => testfunction(entry.id)}>Delete</button>
     </div>
   );
 }
 
+function testfunction(id){
+  console.log(id)
+}
+
+function openEntryCreation() {
+  // Display the overlay and modal
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('modal').style.display = 'block';
+}
+
+function closePopup() {
+  // Hide the overlay and modal
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+}
+
 const entry1 = {
+  id: 1,
   party: "Wallmart",
   description: "Expense for food and drinks",
   time: "2023-01-15T18:30:00",
-  amount: 50.0,
+  amount: -50.0,
+  category: "Food & Drinks",
 };
 
 const entry2 = {
-  party: "Taco Bell",
-  description: "Shopping for groceries",
+  id: 2,
+  party: "Microsoft",
+  description: "Salary January",
   time: "2023-01-16T12:45:00",
-  amount: 30.0,
+  amount: 5000.0,
+  category: "Incomes",
 };
 
 const entry3 = {
-  party: "XNXX",
-  description: "Gas bill payment",
+  id: 3,
+  party: "PornHub",
+  description: "Premium membership",
   time: "2023-01-17T09:15:00",
-  amount: 40.0,
+  amount: -15.0,
+  category: "Entertainment",
 };
 
 const Entries = () => {
@@ -54,6 +79,15 @@ const Entries = () => {
       <Sidebar></Sidebar>
       <div className="content">
         <h1>Entries</h1>
+        <div className="overlay" id="overlay"></div>
+        <div className="modal" id="modal">
+          <span className="close-btn" onClick={() => closePopup()}>
+            &times;
+          </span>
+          <h2>Entry Creation PopUp</h2>
+          <p>This is the content of the popup.</p>
+        </div>
+        <button onClick={() => openEntryCreation()}>Add Entry</button>
         <div className="entry-container">
           <ul>
             {financialEntries.map((entry, index) => (
