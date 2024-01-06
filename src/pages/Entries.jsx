630d52538090ba6e726d6e7c8db379a44f2aa4a2
clemensFH <clemens.hoerhan@stud.fh-campuswien.ac.at>
2024-01-06 10:53:25 +0100

    Add and Delete function

--- src/pages/Entries.jsx
+++ src/pages/Entries.jsx
@@ -4,34 +4,20 @@ import { useState, useEffect } from "react";
 import "../styles/Entries.css";
 //import { getFinancialEntriesFromDB } from "firebase";
 
-function Entry({ entry }) {
-  return (
-    <div className="entry">
-      <h4>{entry.party}</h4>
-      <p>{entry.description}</p>
-      <p>{entry.time}</p>
-      <p>{entry.amount} €</p>
-      <p></p>
-      <button>Edit</button>
-      <button onClick={() => testfunction(entry.id)}>Delete</button>
-    </div>
-  );
-}
-
-function testfunction(id){
-  console.log(id)
+function testfunction(id) {
+  console.log(id);
 }
 
 function openEntryCreation() {
   // Display the overlay and modal
-  document.getElementById('overlay').style.display = 'block';
-  document.getElementById('modal').style.display = 'block';
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("modal").style.display = "block";
 }
 
 function closePopup() {
   // Hide the overlay and modal
-  document.getElementById('overlay').style.display = 'none';
-  document.getElementById('modal').style.display = 'none';
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("modal").style.display = "none";
 }
 
 const entry1 = {
@@ -62,6 +48,21 @@ const entry3 = {
 };
 
 const Entries = () => {
+  
+  function Entry({ entry }) {
+    return (
+      <div className="entry">
+        <h4>{entry.party}</h4>
+        <p>{entry.description}</p>
+        <p>{entry.time}</p>
+        <p>{entry.amount} €</p>
+        <p></p>
+        <button>Edit</button>
+        <button onClick={() => deleteEntry(entry.id)}>Delete</button>
+      </div>
+    );
+  }
+
   const [financialEntries, setFinancialEntries] = useState([]);
 
   useEffect(() => {
@@ -74,6 +75,34 @@ const Entries = () => {
     fetchData();
   }, []);
 
+  function saveEntry() {
+    const party = document.getElementById("party");
+    const amount = document.getElementById("amount");
+    const category = document.getElementById("category");
+    const desc = document.getElementById("description");
+    const time = document.getElementById("time");
+    const newEntry = {
+      id: 99,
+      party: party.value,
+      description: desc.value,
+      time: time.value,
+      amount: amount.value,
+      category: category.value,
+    };
+    let newEntries = [];
+    financialEntries.forEach((item) => {
+      newEntries.push(item);
+    });
+    newEntries.push(newEntry);
+    setFinancialEntries(newEntries);
+    closePopup();
+  }
+
+  function deleteEntry(id) {
+      const newList = financialEntries.filter(item => item.id != id);
+      setFinancialEntries(newList)
+  }
+
   return (
     <div className="Entries">
       <Sidebar></Sidebar>
@@ -84,8 +113,49 @@ const Entries = () => {
           <span className="close-btn" onClick={() => closePopup()}>
             &times;
           </span>
-          <h2>Entry Creation PopUp</h2>
-          <p>This is the content of the popup.</p>
+          <h2>New Income/Expense</h2>
+          <div className="input-container">
+            <label for="party">Party:</label>
+            <input type="text" id="party" name="party" required />
+          </div>
+          <div className="input-container">
+            <label form="amount">Amount:</label>
+            <input
+              type="number"
+              id="amount"
+              name="amount"
+              step="0.01"
+              min="0"
+              placeholder="0.00"
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label for="party">Category:</label>
+            <select id="category" name="category">
+              <option value="volvo">Food & Croceries</option>
+              <option value="saab">Energy</option>
+              <option value="fiat">Leisure & Entertainment</option>
+              <option value="audi">Shopping</option>
+            </select>
+          </div>
+          <p>Optional:</p>
+          <div className="input-container">
+            <label for="description">Description:</label>
+            <input type="text" id="description" name="description" />
+          </div>
+          <div className="input-container">
+            <label for="time">Time:</label>
+            <input type="datetime-local" id="time" name="time" />
+          </div>
+          <div className="input-container">
+            <button type="button" onClick={() => saveEntry()}>
+              Save
+            </button>
+            <button type="button" onClick={() => closePopup()}>
+              Cancel
+            </button>
+          </div>
         </div>
         <button onClick={() => openEntryCreation()}>Add Entry</button>
         <div className="entry-container">
