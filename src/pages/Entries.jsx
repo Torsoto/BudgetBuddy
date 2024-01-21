56add45fff655dba61d5a84d6e0fa1d1e8e70fc6
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2024-01-21 21:21:55 +0100

    Remove unused import and fix duplicate option value

--- src/pages/Entries.jsx
+++ src/pages/Entries.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import EntryForm from "../components/EntryForm"
 import EntriesTable from "../components/EntriesTable";
 import { auth, db } from '../../firebase/firestore.mjs';
